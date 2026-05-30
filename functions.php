bce2c6eff367da8d96c3368a9045a040b04b4b90
futurelithics <chad.denaux@gmail.com>
2026-05-30 16:12:14 -0400

    style: stylsheet

--- functions.php
+++ functions.php
@@ -2,3 +2,9 @@
 
 require_once get_template_directory() . '/lib/init.php';
 
+add_action( 'after_setup_theme', function() {
+    add_theme_support( 'wp-block-styles' );
+    add_theme_support( 'editor-styles' );
+    add_theme_support( 'align-wide' );
+    add_theme_support( 'appearance-tools' );
+});
